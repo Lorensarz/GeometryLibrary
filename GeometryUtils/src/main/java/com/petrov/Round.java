cb1aaf2735f467d7fb41d4b296d1525af9e64d73
Dmitriy Petrov <lorens999@yandex.ru>
2024-05-02 12:55:34 +0300

    Initial commit

---
+++ GeometryUtils/src/main/java/com/petrov/Round.java
@@ -0,0 +1,12 @@
+package com.petrov;
+
+public class Round {
+
+    public static long round(double a) {
+        return Math.round(a);
+    }
+    public static long round(float a) {
+        return Math.round(a);
+    }
+
+}
